535837540684e7d56bbb6b9a8d3dbac95e76b580
Feroz Abdul Rehman <feroz.rehman@salesforce.com>
2019-04-18 20:33:51 +0100

    moved done() to outside chai.end()

--- test/user-controller-tests.js
+++ test/user-controller-tests.js
@@ -10,16 +10,16 @@ describe('User controller tests', () => {
     it('should return a list of items because db always has some data', done => {
       chai
         .request(host)
-        .get('/api/user')
-        .end(function(err, res) {
-          // expect(res).to.have.status(200);
+        .get(`/api/user`)
+        .end((err, res) => {
+          expect(res).to.have.status(200);
           expect(err).to.be.null;
           expect(res).to.be.json;
           expect(res.body[0].id).to.equal('1');
           expect(res.body[0].links.details).to.contain('/api/user/1');
           expect(res.body[0].updatedAt).to.be.undefined;
-          done(); // <= Call done to signal callback end
         });
+      done(); // <= Call done to signal callback end
     });
     it('should return first user using id', done => {
       const userid = '1';
